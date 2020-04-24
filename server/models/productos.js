e6b043da3dc7777767d367e50dd3277a28ba59df
Larry Gonzalez <larry.g.m1024@gmail.com>
2020-04-24 07:21:24 -0300

    subir archivos

--- server/models/productos.js
+++ server/models/productos.js
@@ -4,8 +4,9 @@ var Schema = mongoose.Schema;
 
 var productoSchema = new Schema({
     nombre: { type: String, required: [true, 'El nombre es necesario'] },
-    precioUni: { type: Number, required: [true, 'El precio Ãºnitario es necesario'] },
+    precioUni: { type: Number, required: [true, 'El precio unitario es necesario'] },
     descripcion: { type: String, required: false },
+    img:{ type: String, required: false },
     disponible: { type: Boolean, required: true, default: true },
     categoria: { type: Schema.Types.ObjectId, ref: 'categoria', required: true },
     usuario: { type: Schema.Types.ObjectId, ref: 'Usuario' }
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,7 @@ app.use(require('./login'));
 app.use(require('./usuario'));
 app.use(require('./categorias'));
 app.use(require('./productos'));
+app.use(require('./upload'));
+app.use(require('./imagenes'))
+
 module.exports = app;
